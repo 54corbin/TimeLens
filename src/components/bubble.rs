913fb41143fceffbcfce54052fe9639b44a20c0e
corbin <54corbinchen@gmail.com>
2025-10-12 23:25:26 +1030

    draft water shape

--- src/components/bubble.rs
+++ src/components/bubble.rs
@@ -1,103 +1,181 @@
 use dioxus::prelude::*;
 
-#[derive(Props, Clone, PartialEq)]
+const STYLES: &str = r#"
+    .water-toggle {
+        display: inline-block;
+        position: relative;
+    }
+
+    .container {
+        position: relative;
+        z-index: 1;
+        filter: url(#still-water);
+        animation: wave 10s infinite ease-in-out;
+        transition: 1s;
+        display: block;
+    }
+
+    .container.checked {
+        filter: url(#water-rotate);
+        transition: 0.5s;
+        animation: wave-fast 8s infinite ease-in-out;
+    }
+
+    .water-button {
+        position: relative;
+        z-index: 5;
+        font-size: 1rem;
+        padding: 0;
+        border: none;
+        border-radius: 50%;
+        background: #50626b22;
+        outline: 3px solid #3f545f33;
+        color: #0000008b;
+        text-shadow: 0px 0px 8px #79a5adda;
+        font-weight: bold;
+        backdrop-filter: url(#water) blur(0.4rem);
+        cursor: pointer;
+        font-family: Tahoma, sans-serif;
+        transition: 1s;
+        box-shadow: inset 2px 3px 4px #d4f7fe25;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        white-space: pre-line;
+        text-align: center;
+    }
+
+    .container.checked .water-button {
+        text-shadow: 0px 0px 12px #49e1ffda;
+        background: #13b0ff3d;
+        outline-color: #00aaff00;
+        box-shadow:
+            inset 0px 0px 8px #7ae9ff25,
+            0px 0px 64px #39d9f96f;
+    }
+
+
+    @keyframes wave {
+        0%, 100% { 
+            transform: scale(1);
+        }
+        50% { 
+            transform: scale(1.2);
+        }
+    }
+
+    @keyframes wave-fast {
+        0%, 100% { 
+            transform: scale(1);
+        }
+        50% { 
+            transform: scale(1.3);
+        }
+    }
+"#;
+
+#[derive(Props, PartialEq, Clone)]
 pub struct BubbleProps {
-    /// Size of the SVG (width and height in pixels)
-    #[props(default = 100)]
-    pub size: u32,
+    size: i32,
+    name: String,
+    time: String,
 }
 
 #[component]
 pub fn Bubble(props: BubbleProps) -> Element {
-    let size_str = props.size.to_string();
-    let mut selected = use_signal(|| false);
-    let glow = selected();
+    let mut is_checked = use_signal(|| false);
 
     rsx! {
-        svg {
-            width: "{size_str}",
-            height: "{size_str}",
-            view_box: "0 0 10 10",
-            onclick: move |_| selected.toggle(),
-
-            defs {
-                radialGradient {
-                    id: "myGradient",
-                    cx: "50%",
-                    cy: "50%",
-                    fr: "15%",
-                    r: "50%",
-
-                    stop {
-                        offset: "0%",
-                        stop_color: "#6fff6f",
-                        stop_opacity: "0.0"
-                    }
-                    stop {
-                        offset: "100%",
-                        stop_color: "#6fff6f",
-                        stop_opacity: "0.9"
+        style { {STYLES} }
+        div {
+            class: "inline-block relative",
+            
+            label {
+                class: "water-toggle",
+                
+                input {
+                    r#type: "checkbox",
+                    id: "toggle-wave",
+                    class: "absolute inset-0 w-full h-full opacity-0 cursor-pointer z-30",
+                    checked: is_checked(),
+                    oninput: move |evt| {
+                        is_checked.set(evt.checked());
                     }
                 }
-                if glow {
-                    radialGradient {
-                        id: "glowGradient",
-                        cx: "50%",
-                        cy: "50%",
-                        r: "50%",
-                        fr:"15%",
-                        stop {
-                            offset: "0%",
-                            stop_color: "#6fff6f",
-                            stop_opacity: "0"
+                
+                div {
+                    class: if is_checked() { "container checked" } else { "container" },
+                    
+                    button {
+                        class: "water-button",
+                        style: "width: {props.size}px; height: {props.size}px;",
+                        "{props.name}"
+                        br {}
+                        "{props.time}"
+                    }
+                    
+                    svg {
+                        style: "position: absolute; width: 0; height: 0;",
+                        
+                        filter {
+                            id: "water-rotate",
+                            feTurbulence {
+                                result: "turbulence",
+                                seed: "2",
+                                num_octaves: "2",
+                                base_frequency: "0.0075 0.0075",
+                                r#type: "turbulence"
+                            }
+                            feComponentTransfer {
+                                feFuncR {
+                                    r#type: "table",
+                                    animateTransform {
+                                        repeat_count: "indefinite",
+                                        dur: "10s",
+                                        values: "0.01;0.02;0.01",
+                                        attribute_name: "transform"
+                                    }
+                                }
+                            }
+                            feDisplacementMap {
+                                y_channel_selector: "g",
+                                x_channel_selector: "r",
+                                scale: "35",
+                                in2: "turbulence",
+                                "in": "SourceGraphic"
+                            }
                         }
-                        stop {
-                            offset: "15%",
-                            stop_color: "#6fff6f",
-                            stop_opacity: "1"
-                        }
-                        stop {
-                            offset: "100%",
-                            stop_color: "#6fff6f",
-                            stop_opacity: "0.19"
+                        
+                        filter {
+                            id: "still-water",
+                            feTurbulence {
+                                result: "turbulence",
+                                seed: "2",
+                                num_octaves: "2",
+                                base_frequency: "0.0075 0.0075",
+                                r#type: "turbulence"
+                            }
+                            feComponentTransfer {
+                                feFuncR {
+                                    r#type: "table",
+                                    animateTransform {
+                                        repeat_count: "indefinite",
+                                        dur: "10s",
+                                        values: "0.01;0.02;0.01",
+                                        attribute_name: "transform"
+                                    }
+                                }
+                            }
+                            feDisplacementMap {
+                                y_channel_selector: "g",
+                                x_channel_selector: "r",
+                                scale: "25",
+                                in2: "turbulence",
+                                "in": "SourceGraphic"
+                            }
                         }
                     }
-                    filter {
-                        id: "blurFilter",
-                        feGaussianBlur {
-                            _in: "SourceGraphic",
-                            std_deviation: "0.3"
-                        }
-                    }
-                }
-            }
-
-            if glow {
-                circle {
-                    cx: "5",
-                    cy: "5",
-                    r: "5",
-                    fill: "url(#glowGradient)",
-                    filter: "url(#blurFilter)",
-                    animate {
-                        attribute_name: "r",
-                        values: "4.5;5;4.5",
-                        dur: "4s",
-                        repeat_count: "indefinite"
-                    }
-                }
-            }
-
-            circle {
-                cx: "5",
-                cy: "5",
-                r: "2",
-                fill: "url(#myGradient)",
-                animate {
-                    attribute_name: "r",
-                    values: "2;2.1;2",
-                    dur: "4s",
-                    repeat_count: "indefinite"
                 }
             }
         }
--- src/views/home.rs
+++ src/views/home.rs
@@ -7,10 +7,17 @@ use crate::components::Bubble;
 pub fn Home() -> Element {
     rsx! {
         div {  
-            // background_color: "#6f006f",
+            background_color: "#f0f0f0",
 
-            Bubble {size:59}
-            Bubble {size:291}
+            Bubble { size: 150, name: "Test 2Test 2Test Home aniimation long long onlngl hlahllj ahljaliln".to_string(), time: "08:11:00".to_string() }
+            // Bubble { size: 50, name: "Test 1".to_string(), time: "10:00".to_string() }
+            Bubble { size: 100, name: "Test 2Test 2Test 2".to_string(), time: "11:00".to_string() }
+            // Bubble { size: 150, name: "Test 3".to_string(), time: "12:00".to_string() }
+            // Bubble { size: 200, name: "Test 4".to_string(), time: "13:00".to_string() }
+            // Bubble { size: 250, name: "Test 5".to_string(), time: "14:00".to_string() }
+            // Bubble { size: 300, name: "Test 6".to_string(), time: "15:00".to_string() }
+            // Bubble { size: 350, name: "Test 7Test 8".to_string(), time: "16:00".to_string() }
+            // Bubble { size: 350, name: "Test 8Test 8Test 8".to_string(), time: "17:00".to_string() }
         }
     }
 }
